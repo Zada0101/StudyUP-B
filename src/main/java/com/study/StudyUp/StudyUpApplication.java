e486c71cec936b9c80778f51148a6ef6588384d4
Zada <zadabaktybek07@gmail.com>
2025-09-19 22:14:08 -0700

    Spring boot run

---
+++ src/main/java/com/study/StudyUp/StudyUpApplication.java
@@ -0,0 +1,13 @@
+package com.study.StudyUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyUpApplication.class, args);
+	}
+
+}
