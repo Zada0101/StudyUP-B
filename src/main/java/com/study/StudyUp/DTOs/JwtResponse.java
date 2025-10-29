a998340f8b2fc12b80b5d6260a3ed05634216cd6
Zada <zadabaktybek07@gmail.com>
2025-10-29 10:09:59 -0700

    Chat

---
+++ src/main/java/com/study/StudyUp/DTOs/JwtResponse.java
@@ -0,0 +1,12 @@
+package com.study.StudyUp.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String username;
+    private String message;
+}
