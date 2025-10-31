29bd84a42fbbf95827e4d63cfc85b9ac4c5a10cd
Zada <zadabaktybek07@gmail.com>
2025-10-30 23:11:18 -0700

    html

--- src/main/java/com/study/StudyUp/StudyUpApplication.java
+++ src/main/java/com/study/StudyUp/StudyUpApplication.java
@@ -3,11 +3,10 @@ package com.study.StudyUp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.study.StudyUp")
 public class StudyUpApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StudyUpApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(StudyUpApplication.class, args);
+        System.out.println("✅ StudyUp server is running...");
+    }
 }
