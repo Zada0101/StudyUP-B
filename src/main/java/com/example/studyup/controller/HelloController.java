74532f44738f46899fc7ef389a0aac7676cbb950
Zada <zadabaktybek07@gmail.com>
2025-12-13 14:53:59 -0800

    HelloController

--- src/main/java/com/example/studyup/controller/HelloController.java
+++ src/main/java/com/example/studyup/controller/HelloController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * ✅ Simple controller to confirm the server is working.
  */
+
+//This annotation tells to Spring boot
+// this class should receive HTTP requests and send responses
 @RestController
 public class HelloController {
 
+
+    //@GetMapping->is an annotation that tells Spring when to run a method; the method contains the code that runs
+    //home()->Method	Runs code and returns response
     @GetMapping("/")
     public String home() {
         return "🚀 StudyUp backend is running successfully!";
